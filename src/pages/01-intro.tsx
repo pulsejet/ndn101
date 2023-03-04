6793fd06c11ca7f00bbea72941474383a170b1f5
Varun Patil <radialapps@gmail.com>
2023-03-04 18:03:45 -0500

    Add mcast page

--- src/pages/01-intro.tsx
+++ src/pages/01-intro.tsx
@@ -7,30 +7,36 @@ export default function Page() {
             <h1 className="title is-3">
                 Named Data Networking &#8211; An Introduction
             </h1>
-            <h3>
-                What is NDN ?
-            </h3>
-            <p>
-            The Internet's architecture, with its simple and universal network layer (IP), 
-            enabled its explosive growth, but its design choices are the root cause of today's Internet problems, 
-            especially when it comes to distribution networks.
-            </p> 
-            <p>
-            Named Data Networking (NDN) retains the Internet's hourglass architecture 
-            but allows the creation of completely general distribution networks by removing 
-            the restriction that packets can only name communication endpoints. 
-            This enables NDN networks to solve not only communication problems but also digital distribution 
-            and control problems. 
-            The NDN project's research challenge is to evolve it into an architectural framework capable of solving real problems,
-             particularly in areas poorly served by today's Internet, through an experimental research effort that verifies and shapes the architectural direction.
-            </p>
-            <h3>
-                Why NDN ?
-            </h3>
-            <Why/>
-            <h3>
+
+            <div>
+                <p>
+                    The Internet's architecture, with its simple and universal network layer (IP),
+                    enabled its explosive growth, but its design choices are the root cause of
+                    today's Internet problems, especially when it comes to distribution networks.
+                </p>
+
+                <p>
+                    Named Data Networking (NDN) retains the Internet's hourglass architecture
+                    but allows the creation of completely general distribution networks by removing
+                    the restriction that packets can only name communication endpoints.
+                    This enables NDN networks to solve not only communication problems but also digital distribution
+                    and control problems.
+
+                    The NDN project's research challenge is to evolve it into an architectural framework capable
+                    of solving real problems, particularly in areas poorly served by today's Internet, through
+                    an experimental research effort that verifies and shapes the architectural direction.
+                </p>
+            </div>
+
+            <h2 className="title is-4">
+                Why bother with NDN?
+            </h2>
+
+            <Why />
+
+            <h2 className="title is-4">
                 How to NDN ?
-            </h3>
+            </h2>
         </div>
     );
 }
---
+++ src/pages/04-multicast.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+export default function Page() {
+    return (
+        <div className="content">
+            <h1 className="title is-3">
+                Data Multicast
+            </h1>
+
+            Requesting data by Name directly and naturally results in multicast data delivery.
+            The sections below describe this key feature of NDN.
+
+            <h2 className="title is-4">
+                Interest Aggregation
+            </h2>
+        </div>
+    );
+}
